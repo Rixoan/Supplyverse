845238e341b01ed7d111c3e6e2a4007d3db5ca4b
MuhammadAl-FajriSilmi <191111860@students.mikroskil.ac.id>
2023-01-22 11:33:39 +0700

    FiturBayarPakaiCardCredit

--- app/src/main/java/com/example/test2/UI/Cart_DetailActivity.kt
+++ app/src/main/java/com/example/test2/UI/Cart_DetailActivity.kt
@@ -2,10 +2,12 @@ package com.example.test2.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.test2.EXTRA_SUPPLIER
 import com.example.test2.Models.SupplierDataModel
 import com.example.test2.R
 import kotlinx.android.synthetic.main.activity_cart_detail.*
+import java.util.*
 
 
 class Cart_DetailActivity : AppCompatActivity() {
@@ -30,6 +32,33 @@ class Cart_DetailActivity : AppCompatActivity() {
         tv_cart_detail_harga.text = listharga
         tv_cart_detail_total.text = total.toString()
 
+        button_checkout.setOnClickListener {
+            if (rb_kredit.isChecked) {
+                Toast.makeText(
+                    this@Cart_DetailActivity, "Kamu berhasil melakukan pembayaran dengan kredit ",
+                    Toast.LENGTH_SHORT
+                ).show()
+                historilist.add(
+                    HistoriDataModel(intent_supplier!!,Calendar.getInstance().time.toString(),
+                        HistoriAdapter.VIEW_TYPE_SELESAI)
+                )
+
+            }
+            removefromcart()
+            finish()
+        }
 
     }
+    fun removefromcart(){
+        var index = 0
+        for(i in cart){
+            if(i.nama == intent_supplier?.nama){
+                cart.removeAt(index)
+                break
+
+            }
+            index++
+        }
+    }
+
 }
